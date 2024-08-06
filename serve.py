f0062583d801a550a0c3074451b5210d9003259e
Daeseong Kim <16497919+dkimds@users.noreply.github.com>
2024-08-06 16:13:59 +0900

    Update serve.py

--- serve.py
+++ serve.py
@@ -3,55 +3,25 @@ from typing import List
 
 from fastapi import FastAPI
 from langchain_core.prompts import ChatPromptTemplate
-# from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langserve import add_routes
 
-from langchain_community.document_loaders import PyPDFLoader
-
-file_path = "./example_data/2408.00714v1.pdf"
-loader = PyPDFLoader(file_path)
-
-docs = loader.load()
-
-# 1. Create prompt 
-system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
-    "answer concise."
-    "\n\n"
-    "{context}"
-)
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
+# 1. Create prompt template
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
 
 # 2. Create model
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+model = ChatOpenAI()
 
-# # 3. Create retriever
-from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-
-retriever = vectorstore.as_retriever()
+# 3. Create parser
+parser = StrOutputParser()
 
 # 4. Create chain
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-question_answer_chain = create_stuff_documents_chain(llm, prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+chain = prompt_template | model | parser
 
 
 # 4. App definition
@@ -65,7 +35,7 @@ app = FastAPI(
 
 add_routes(
     app,
-    rag_chain,
+    chain,
     path="/chain",
 )
 
